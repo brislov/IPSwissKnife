db1b38b96a1db61285c674f9bf91eaaf76fc1931
brislov <markusbrislov@gmail.com>
2022-01-31 23:37:07 +0100

    ConnectionViewModel stuff

--- IPSwissKnife/ViewModels/ConnectionViewModel.cs
+++ IPSwissKnife/ViewModels/ConnectionViewModel.cs
@@ -4,9 +4,26 @@ namespace IPSwissKnife.ViewModels
 {
     internal class ConnectionViewModel : BindableObject
     {
+        private string addressEntry;
+        private string portEntry;
+        internal string AddressEntry 
+        { 
+            get => addressEntry; 
+            set => addressEntry = value;
+        }
+        internal string PortEntry 
+        { 
+            get => portEntry; 
+            set => addressEntry = value;
+        }
+
         internal ICommand CreateConnectionCommand { get; }
         internal void CreateConnectionMethod()
         {
+            if (string.IsNullOrEmpty(addressEntry) && string.IsNullOrEmpty(portEntry))
+            {
+                return;
+            }
 
         }
     }
